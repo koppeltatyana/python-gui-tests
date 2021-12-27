c418f38b1df408dd460dac0f5812342322294685
Tatyana Koppel <tatyana.koppel@mail.ru>
2021-12-27 18:07:18 +0300

    Реализован тест для удаления группы (пока что не рабочий)

---
+++ tests/test_del_group.py
@@ -0,0 +1,10 @@
+import random
+
+
+def test_del_group(app):
+    old_list = app.group.get_group_list()
+    random_group = random.choice(old_list)
+    app.group.del_some_group(random_group)
+    new_list = app.group.get_group_list()
+    old_list.remove("new_new_group")
+    assert sorted(old_list) == sorted(new_list)

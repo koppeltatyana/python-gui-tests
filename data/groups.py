11867dc4776d82f078bbdf78522566a76ee49d14
Tatyana Koppel <tatyana.koppel@mail.ru>
2021-12-29 14:55:33 +0300

    Раелизован метод генерации тестовых данных в файл data/groups.xlsx

---
+++ data/groups.py
@@ -0,0 +1,4 @@
+test_data = [
+    "group 4",
+    "group 5"
+]
--- generator/group.py
+++ generator/group.py
@@ -1,13 +1,19 @@
-from comtypes.client import CreateObject
 import os
 import win32com.client
+import random
+import string
+
+
+def random_string(maxlen):
+    symbols = string.ascii_letters + string.digits
+    return "".join(random.choice(symbols) for _ in range(random.randrange(maxlen)))
+
 
 project_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + "\\data"
-print("\n", "project_dir = ", project_dir)
 xl = win32com.client.Dispatch("Excel.Application")
 xl.Visible = 1
 wb = xl.Workbooks.Add()
 for i in range(10):
-    xl.Range("A{}".format(i+1)).Value = "group {}".format(i)
+    xl.Range("A{}".format(i+1)).Value = random_string(15)
 wb.SaveAs(os.path.join(project_dir, "groups.xlsx"))
 xl.Quit()

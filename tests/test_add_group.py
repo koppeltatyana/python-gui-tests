5bc3793921c8a060e5bc3391679c3c2f3f5aa536
Tatyana Koppel <tatyana.koppel@mail.ru>
2021-12-29 14:56:51 +0300

    Корректировка теста добавления группы (добавлена динамическая параметризация и проверка на пустую строку входных данных)

--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -1,6 +1,10 @@
-def test_add_group(app):
+def test_add_group(app, xlsx_groups):
+    new_group = xlsx_groups
     old_list = app.group.get_group_list()
-    app.group.add_new_group("new_new_group")
+    app.group.add_new_group(new_group)
     new_list = app.group.get_group_list()
-    old_list += ["new_new_group"]
+    if new_group == "":
+        old_list += ["New group"]
+    else:
+        old_list += [new_group]
     assert sorted(old_list) == sorted(new_list)
